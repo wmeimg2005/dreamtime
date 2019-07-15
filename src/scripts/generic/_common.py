720647896d885178ba697978d979bd3014a22e6d
PommeDroid <u11Ag65iop1a5OyAngBA@protonmail.com>
2019-07-15 15:14:10 +0200

    Add generic python setup and install scripts

---
+++ src/scripts/generic/_common.py
@@ -0,0 +1,68 @@
+import logging
+import os
+import shutil
+import sys
+import tempfile
+from contextlib import contextmanager
+from enum import Enum
+from sys import platform as _platform
+
+
+class OS(Enum):
+    UNKNOW = "unknown"
+    LINUX = "linux"
+    MAC = "mac"
+    WIN = "win"
+
+
+def get_os():
+    if _platform == "linux" or _platform == "linux2":
+        return OS.LINUX
+    elif _platform == "darwin":
+        return OS.MAC
+    elif _platform == "win32" or _platform == "win64":
+        return OS.WIN
+    else:
+        return OS.UNKNOW
+
+
+def get_python_version():
+    return sys.version_info[0], sys.version_info[1]
+
+
+def check_node():
+    return shutil.which("node")
+
+
+def check_yarn():
+    return shutil.which("yarn")
+
+
+def check_pyinstaller():
+    return shutil.which("pyinstaller")
+
+
+def setup_log(log_lvl=logging.DEBUG):
+    log = logging.getLogger(__name__)
+    out_hdlr = logging.StreamHandler(sys.stdout)
+    out_hdlr.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+    log.addHandler(out_hdlr)
+    log.setLevel(log_lvl)
+    return log
+
+
+@contextmanager
+def cd(newdir):
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        yield
+    finally:
+        os.chdir(prevdir)
+
+
+def create_temporary_copy(path, copy_name):
+    temp_dir = tempfile.gettempdir()
+    temp_path = os.path.join(temp_dir, copy_name)
+    shutil.copy2(path, temp_path)
+    return temp_path
---
+++ src/scripts/generic/build.py
@@ -0,0 +1,91 @@
+import argparse
+import logging
+
+import subprocess
+import PyInstaller.__main__
+import shutil
+from _common import get_os, setup_log, OS, get_python_version, cd, check_node, check_yarn, check_pyinstaller
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-d', '--debug', action='store_true',
+                    help='Set log level to Debug')
+
+args = parser.parse_args()
+log = setup_log(logging.DEBUG if args.debug else logging.INFO)
+
+## System & Dependencies Check
+detected_os = get_os()
+detected_py = get_python_version()
+log.debug("OS : {}".format(detected_os))
+log.debug("Python version : {}".format(detected_py))
+
+if detected_os == OS.UNKNOW:
+    log.fatal("Unknown OS !")
+    exit(1)
+
+if detected_py < (3, 5):
+    log.fatal("Unsupported python version !")
+    exit(1)
+
+if not check_node():
+    log.fatal("NodeJs is not install. It's a required dependency !")
+    exit(1)
+
+if not check_yarn():
+    log.fatal("Yarn is not install. It's a required dependency !")
+    exit(1)
+
+
+if not check_pyinstaller():
+    log.fatal("Pyinstaller is not install. It's a required dependency !")
+    exit(1)
+
+## Build Cli
+log.info('Building Cli')
+with cd("../../cli"):
+    try:
+        PyInstaller.__main__.run([
+            '-y',
+            '--onedir',
+            '--name=cli',
+            '--distpath=../../dist',
+            'main.py',
+        ])
+    except Exception as e:
+        log.error(e)
+        log.fatal("Cli building failed")
+        exit(1)
+log.info('Cli successfully built')
+
+## Build Gui
+log.info('Building Cli')
+with cd("../../gui"):
+    r = subprocess.run(['yarn', 'build'], shell=True) if detected_os == OS.WIN else subprocess.run(['yarn', 'build'])
+    if r.returncode != 0:
+        log.fatal("Cli building failed")
+        exit(1)
+log.info('Cli successfully built')
+
+
+## Move Generated Files
+def gui_build_files_location():
+    if detected_os == OS.LINUX:
+        return './gui-unpacked/linux-unpacked'
+    if detected_os == OS.MAC:
+        log.warning('Gui files path location not tested on mac')
+        return './gui-unpacked/linux-unpacked'
+    if detected_os == OS.WIN:
+        return './gui-unpacked/win-unpacked'
+
+
+log.info('Moving the build files to final location')
+with cd("../../../dist"):
+    shutil.rmtree('./gui', ignore_errors=True)
+    shutil.move(gui_build_files_location(), "./gui")
+    shutil.rmtree('./gui-unpacked', ignore_errors=True)
+
+log.info('Build to Final Directory')
+
+log.info('Build completed!')
+log.info('It should have generated a folder called dist/, inside you will find the final project files that you can share with everyone!')
+log.info('Enjoy and remember to respect the License!')
