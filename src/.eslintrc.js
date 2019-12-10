364e6050e74cd0c34c79220e9f0767c357d538d7
Ivan Bravo Bravo <ivan@dreamnet.tech>
2019-12-09 21:08:15 -0600

    Linebreak and lint fix.

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -57,6 +57,7 @@ module.exports = {
     "lodash/prefer-lodash-method": "off",
     "lodash/prefer-lodash-typecheck": "warn",
     "lodash/prefer-noop": "off",
+    "import/extensions": "off",
     "max-len": "off",
     "func-names": "off",
     "no-await-in-loop": "warn",
